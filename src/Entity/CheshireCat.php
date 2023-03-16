761ab472cb0fc0d44c90d95b8f0c78fd183a4010
Thomas di Luccio <thomas.diluccio@platform.sh>
2023-03-16 15:17:26 +0100

    add space

--- src/Entity/CheshireCat.php
+++ src/Entity/CheshireCat.php
@@ -8,7 +8,7 @@ class CheshireCat
     public function followMe(): string
     {
         /**
-         * Call-graphs comes in multiple flavours for multiple dimensions.
+         * Call-graphs come in multiple flavours for multiple dimensions.
          * This call is clearly visible on the CPU one.
          * Click on the "Processor" icon on the top bar
          * https://blackfire.io/docs/profiling-cookbooks/understanding-call-graphs
